1556a1ea17f041728cf0de69fe856d494602d75b
johanna <johanna.kull@live.de>
2024-07-22 12:26:56 +0100

    add todo list

---
+++ main.py
@@ -0,0 +1,8 @@
+# TODO: create snake body
+# TODO: move snake body
+# TODO: steer snake
+# TODO: detect collision with food
+# TODO: keep track of score
+# TODO: detect collision with wall
+# TODO: detect snake's collision with itself
+
