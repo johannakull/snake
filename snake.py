5eb240ee11ff71da2e95036e5300d38343bd3133
johanna <johanna.kull@live.de>
2024-07-22 13:38:45 +0100

    more refactor

--- snake.py
+++ snake.py
@@ -6,6 +6,9 @@ STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 class Snake:
     def __init__(self):
         self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
         for position in STARTING_POSITIONS:
             snake_segment = Turtle(shape="square")
             snake_segment.color("white")
