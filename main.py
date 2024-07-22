6d73f0b5fe1c29142c3d5437cc1baacb9e9e849a
johanna <johanna.kull@live.de>
2024-07-22 13:36:02 +0100

    refactor part 2 - create move method for snake

--- main.py
+++ main.py
@@ -16,13 +16,7 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for segment_num in range(len(snake.segments) - 1, 0, -1): # start, stop, step - note that it will stop immediately once it reaches stop value
-        # get the coordinates of the segment ahead of the current segment
-        new_x = snake.segments[segment_num - 1].xcor()
-        new_y = snake.segments[segment_num - 1].ycor()
-        # move the segment to the new coordinates
-        snake.segments[segment_num].goto(new_x, new_y)
-    snake.segments[0].forward(20)
+    snake.move()
 
 # TODO: steer snake
 # TODO: detect collision with food
--- snake.py
+++ snake.py
@@ -13,3 +13,12 @@ class Snake:
             snake_segment.goto(position)
             self.segments.append(snake_segment)
 
+    def move(self):
+        for segment_num in range(len(self.segments) - 1, 0,
+                                 -1):  # start, stop, step - note that it will stop immediately once it reaches stop value
+            # get the coordinates of the segment ahead of the current segment
+            new_x = self.segments[segment_num - 1].xcor()
+            new_y = self.segments[segment_num - 1].ycor()
+            # move the segment to the new coordinates
+            self.segments[segment_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
