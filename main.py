ec574e22ce8839e201aa2cb5e009851864b42a00
johannakull <johanna.kull@live.de>
2024-07-23 23:26:12 +0100

    display and update score during game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from scoreboard import Scoreboard
 from food import Food
 import time
 
@@ -11,10 +12,11 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
-screen.onkey(snake.down,"Down")
+screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
@@ -27,6 +29,7 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 # TODO: keep track of score
 # TODO: detect collision with wall
---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.setpos(0, 270)
+        self.color("white")
+        self.hideturtle()
+        self.display_score()
+
+    def display_score(self):
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 18, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.display_score()
+        
